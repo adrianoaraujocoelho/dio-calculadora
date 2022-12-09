293cf11cfed5bfb5de03631254f8ac6717040449
adriano.dev <adriano.coelho@dataside.com.br>
2022-12-09 20:39:42 -0300

    dio-calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #5ae288;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 30px;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #5ae288;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #ffffff;
+  }
+`;
